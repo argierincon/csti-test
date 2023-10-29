d8c95eb8b53a33c96566a39a1cb73462eb5a6f71
argierincon <argierincon@gmail.com>
2023-10-28 22:21:53 -0500

    feat: adding project icons

---
+++ src/components/icons/Plus.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg
+    :width="sizeIcon(size)"
+    :height="sizeIcon(size)"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M12 4C12.5523 4 13 4.44772 13 5V19C13 19.5523 12.5523 20 12 20C11.4477 20 11 19.5523 11 19V5C11 4.44772 11.4477 4 12 4Z"
+      :fill="fillIcon(color)"
+    />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M4 12C4 11.4477 4.44772 11 5 11H19C19.5523 11 20 11.4477 20 12C20 12.5523 19.5523 13 19 13H5C4.44772 13 4 12.5523 4 12Z"
+      :fill="fillIcon(color)"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { IIconProps } from "./interface";
+import { fillIcon, sizeIcon } from "../../utils/setIconStyles";
+
+const props = withDefaults(defineProps<IIconProps>(), {
+  color: "black",
+  size: "medium",
+});
+</script>
---
+++ src/components/icons/Search.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg
+    :width="sizeIcon(size)"
+    :height="sizeIcon(size)"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g id="Search">
+      <g id="Vector">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M11.7664 3.52802C7.21636 3.52802 3.52783 7.21654 3.52783 11.7666C3.52783 16.3166 7.21636 20.0051 11.7664 20.0051C16.3164 20.0051 20.0049 16.3166 20.0049 11.7666C20.0049 7.21654 16.3164 3.52802 11.7664 3.52802ZM2.02783 11.7666C2.02783 6.38812 6.38793 2.02802 11.7664 2.02802C17.1448 2.02802 21.5049 6.38812 21.5049 11.7666C21.5049 17.145 17.1448 21.5051 11.7664 21.5051C6.38793 21.5051 2.02783 17.145 2.02783 11.7666Z"
+          :fill="fillIcon(color)"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M17.4871 17.9555C17.7796 17.6622 18.2545 17.6616 18.5477 17.9541L22.0718 21.469C22.365 21.7615 22.3657 22.2363 22.0732 22.5296C21.7806 22.8229 21.3058 22.8235 21.0125 22.531L17.4885 19.0161C17.1952 18.7236 17.1946 18.2487 17.4871 17.9555Z"
+          :fill="fillIcon(color)"
+        />
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { IIconProps } from "./interface";
+import { fillIcon, sizeIcon } from "../../utils/setIconStyles";
+
+const props = withDefaults(defineProps<IIconProps>(), {
+  color: "black",
+  size: "medium",
+});
+</script>
