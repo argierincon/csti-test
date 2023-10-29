64da949d2cf0bde0f535bf2695ebda59242d4471
argierincon <argierincon@gmail.com>
2023-10-28 22:20:32 -0500

    refactor: adding dynamic color

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -10,13 +10,23 @@
       cx="12"
       cy="12"
       r="10"
-      stroke="white"
+      stroke="grey"
       stroke-width="4"
     ></circle>
     <path
       class="opacity-75"
-      fill="white"
+      :fill="color"
       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
     ></path>
   </svg>
 </template>
+
+<script setup lang="ts">
+export interface Props {
+  color: "black" | "white";
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  color: "white",
+});
+</script>
