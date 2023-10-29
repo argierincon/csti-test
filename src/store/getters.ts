83cfd4e200db5b20a5c53d9a2587ce7eedf5b9c4
argierincon <argierincon@gmail.com>
2023-10-29 13:29:57 -0500

    feat: implement getters

---
+++ src/store/getters.ts
@@ -0,0 +1,12 @@
+import { IState } from "./state";
+export const getters = {
+  token(state: IState) {
+    return state?.dataAuth?.data?.token;
+  },
+  user(state: IState) {
+    return state?.dataAuth?.data?.user;
+  },
+  employees(state: IState) {
+    return state?.dataEmployees?.data;
+  },
+};
