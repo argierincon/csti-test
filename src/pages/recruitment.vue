f56904fa4c7a7710ee504319c3b3ab370b65f5bc
argierincon <argierincon@gmail.com>
2023-10-28 22:24:36 -0500

    feat: adding recruitment page

---
+++ src/pages/recruitment.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="main-sections">
+    <div class="info">
+      <Icon name="infoCircle" color="blue" size="large" />
+      <p>Recruitment page in progress</p>
+      <p class="text-4xl">👩🏻‍💻</p>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import Icon from "../components/Icon.vue";
+</script>
+
+<style lang="postcss" scoped>
+.info {
+  @apply flex items-center gap-x-1 text-base text-info-500;
+}
+</style>
