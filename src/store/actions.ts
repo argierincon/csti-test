dc457546bc1d9f1474616dca45300bba55c78d8e
argierincon <argierincon@gmail.com>
2023-10-29 13:29:39 -0500

    feat: implement actions for api integration

---
+++ src/store/actions.ts
@@ -0,0 +1,22 @@
+import { http } from "../services/http";
+import { IAuth, IAuthPayload, IData } from "./interfaces/auth.interface";
+import { IEmpleados } from "./interfaces/employee.interface";
+
+export const actions = {
+  async login(payload: IAuthPayload) {
+    const { data } = await http.post<IAuth>("/auth/login", payload);
+    console.log(data);
+    http.defaults.headers.common.Authorization = `Token ${data.data.token}`;
+    this.dataAuth = data;
+  },
+  async getEmployees() {
+    const queryParams = `limit=${this.tableLimit}&page=${this.tablePage}`;
+    const { data } = await http.get<IEmpleados>("/empleados?" + queryParams);
+    this.dataEmployees = data;
+  },
+
+  setAuthData(data: IData) {
+    http.defaults.headers.common.Authorization = `Token ${data.token}`;
+    this.dataAuth = { data };
+  },
+};
