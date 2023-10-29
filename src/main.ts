188c66238fb6d41d22f4805bee2e52a50cbd37e7
argierincon <argierincon@gmail.com>
2023-10-28 22:12:15 -0500

    refactor: organizing module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).mount('#app')
+import Blank from "./layouts/Blank.vue";
+import Default from "./layouts/Default.vue";
+import { setIcons } from "./components/icons";
+
+const app = createApp(App);
+app.use(router);
+app.component("default-layout", Default);
+app.component("blank-layout", Blank);
+setIcons(app);
+app.mount("#app");
