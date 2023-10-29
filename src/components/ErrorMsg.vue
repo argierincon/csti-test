4b3bff0fe8b2447082886b2d5fadd06fd21ae168
argierincon <argierincon@gmail.com>
2023-10-28 22:16:57 -0500

    feat: adding error msg component

---
+++ src/components/ErrorMsg.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="error-msg">
+    <Icon :name="icon" color="red" size="small" />
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Icon from "./Icon.vue";
+
+interface Props {
+  msg: string;
+  icon: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  msg: "Correo o contraseña incorrectos",
+  icon: "infoCircle",
+});
+</script>
+
+<style lang="postcss" scoped>
+.error-msg {
+  @apply flex items-center gap-x-1 text-xs text-error-500;
+}
+</style>
