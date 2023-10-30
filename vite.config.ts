f41909e4c60bb96f2e1a0b2f56c7fe293d48b411
argierincon <argierincon@gmail.com>
2023-10-29 19:22:37 -0500

    test: set base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/csti-test/",
+  base: "https://argierincon.github.io/csti-test",
   plugins: [vue()],
 })
