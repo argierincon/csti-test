ead407887fab02c7ab0cb27bd9f8c7d406d89e9e
argierincon <argierincon@gmail.com>
2023-10-30 06:49:12 -0500

    test: adding .env log

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+console.log(import.meta.env, "VITE_API_BASE_URL");
 export const http = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: { "Content-Type": "application/json" },
