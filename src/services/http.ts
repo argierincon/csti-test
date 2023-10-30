74d634d86a553a2f1ec008fd7b826b8c2f688265
argierincon <argierincon@gmail.com>
2023-10-30 07:11:41 -0500

    refactor: removing .env log

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-console.log(import.meta.env, "VITE_API_BASE_URL");
 export const http = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: { "Content-Type": "application/json" },
