547828f45fafee6f7d67b1b6fa765547a67c67e1
argierincon <argierincon@gmail.com>
2023-11-02 15:48:20 -0500

    refactor: update some styles

--- src/pages/recruitment.vue
+++ src/pages/recruitment.vue
@@ -1,11 +1,13 @@
 <template>
   <section class="main-sections flex flex-col justify-center">
     <div class="info">
-      <Icon name="infoCircle" size="large" />
-      <p>Página de reclutamiento en progreso.</p>
+      <div class="flex items-start gap-x-1 md:items-center">
+        <Icon name="infoCircle" size="large" />
+        <p>Página de reclutamiento en progreso.</p>
+      </div>
       <p class="text-4xl">👩🏻‍💻</p>
     </div>
-    <div class="info">
+    <div class="info mt-8">
       <p>Por favor, vuelve mas tarde.</p>
       <p class="text-4xl">🤓</p>
     </div>
@@ -18,6 +20,7 @@ import Icon from "../components/Icon.vue";
 
 <style lang="postcss" scoped>
 .info {
-  @apply flex items-center justify-center gap-x-1 text-base;
+  @apply flex flex-col items-center justify-center gap-x-1 text-base;
+  @apply md:flex-row;
 }
 </style>
