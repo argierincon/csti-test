195d999679602b1fdc0857c54837ed16a4ca2a3c
argierincon <argierincon@gmail.com>
2025-01-14 16:54:03 -0500

    refactor: remove unused state

--- src/store/state.ts
+++ src/store/state.ts
@@ -2,15 +2,11 @@ import { IAuth } from "./interfaces/auth.interface";
 import { IEmpleados } from "./interfaces/employee.interface";
 
 export interface IState {
-  dataAuth     : IAuth;
+  dataAuth: IAuth;
   dataEmployees: IEmpleados;
-  tableLimit   : number;
-  tablePage    : number;
 }
 
 export const state = (): IState => ({
-  dataAuth     : null,
+  dataAuth: null,
   dataEmployees: null,
-  tableLimit   : 10,
-  tablePage    : 1,
 });
